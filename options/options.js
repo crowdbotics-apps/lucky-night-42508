49d0c8f452f6cbb8418584d75d316db2a45b0229
Crowdbotics <team@crowdbotics.com>
2023-07-29 11:38:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-night-42508",
+  url: "https://lucky-night-42508.botics.co",
+  api: "https://lucky-night-42508.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
